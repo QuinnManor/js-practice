c9127c574d33dcb885a1f54d1be473ec53fae0e9
QuinnManor <matthewq1428@gmail.com>
2022-03-30 20:26:51 -0400

    Add poetry club problem

---
+++ 5.poetry-club-door-policy/door-policy.js
@@ -0,0 +1,15 @@
+function frontDoorResponse(line) {
+  throw new Error("Remove this line and implement the function");
+}
+
+function frontDoorPassword(word) {
+  throw new Error("Remove this line and implement the function");
+}
+
+function backDoorResponse(line) {
+  throw new Error("Remove this line and implement the function");
+}
+
+function backDoorPassword(word) {
+  throw new Error("Remove this line and implement the function");
+}
